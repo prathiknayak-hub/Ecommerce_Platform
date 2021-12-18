958c1249aeea4708397f301c3271ecb6f8a26373
nagaraj2507 <92426290+nagaraj2507@users.noreply.github.com>
2021-12-18 11:33:51 +0530

    quantity and search

--- router/dashboard.js
+++ router/dashboard.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
+const bodyParser=require("body-parser");
+
 router.get('/',(req,res) => res.render('dashboard', { layout: false }));
+
+router.post("/search",(req,res)=>{
+    const b=req.body.search;
+    if(b=="mobile"){
+        res.render("mobile");
+    }else if(b=="laptop"){
+        res.render("laptop");
+    }else{
+        res.render("dashboard");
+    }
+});
 
 module.exports = router;
